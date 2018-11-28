c5ad867d2b532d9b0489fca631aa7655ca75aa24
Campbell Vertesi <campbell@vertesi.com>
2018-11-28 17:54:41 +0100

    Async function

--- azure-functions-ynab-sync/index.js
+++ azure-functions-ynab-sync/index.js
@@ -1,6 +1,6 @@
 const app = require('../index.js')
 
-module.exports = function (context, req) {
-  await app.doIt(req, context.res);
-  context.done();
+module.exports = async (context, req) => {
+  await app.doIt(req, context.res)
+  context.done()
 }
